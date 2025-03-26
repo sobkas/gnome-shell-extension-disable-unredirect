5c3ae71a4960a01a2d490621f6a8e211fdcdcb74
Krzysztof Sobiecki <sobkas@sobkas.io>
2025-03-26 21:00:50 +0100

    Add Gnome 48 support

--- unredirect@vaina.lt/extension.js
+++ unredirect@vaina.lt/extension.js
@@ -2,28 +2,28 @@ import Meta from 'gi://Meta';
 
 export default class UnredirectExtension {
     constructor() {
-        this.orig_enable_unredirect_for_display = null;
+        this.orig_enable_unredirect = null;
     }
 
     enable() {
-        if (this.orig_enable_unredirect_for_display == null) {
+        if (this.orig_enable_unredirect == null) {
             console.debug("Monkey patching Meta.enable_unredirect_for_display(display) function");
-            this.orig_enable_unredirect_for_display = Meta.enable_unredirect_for_display;
-            Meta.enable_unredirect_for_display = function(display) {
+            this.orig_enable_unredirect = global.compositor.enable_unredirect;
+            global.compositor.enable_unredirect = function(display) {
                 // should I check for display?
-                console.debug("Ignoring invocation of Meta.enable_unredirect_for_display(display)");
+                console.debug("Ignoring invocation of Meta.Compositor.enable_unredirect(display)");
             };
-            Meta.disable_unredirect_for_display(global.display);
+            global.compositor.disable_unredirect(global.display);
         }
     }
 
     disable() {
-        if (this.orig_enable_unredirect_for_display != null) {
+        if (this.orig_enable_unredirect != null) {
             console.debug("Restoring Meta.enable_unredirect_for_display(display) function");
-            Meta.enable_unredirect_for_display = this.orig_enable_unredirect_for_display;
-            this.orig_enable_unredirect_for_display = null;
-            console.debug("Invoking Meta.enable_unredirect_for_display(display)");
-            Meta.enable_unredirect_for_display(global.display);
+            global.compositor.enable_unredirect = this.orig_enable_unredirect;
+            this.orig_enable_unredirect = null;
+            console.debug("Invoking Meta.Compositor.enable_unredirect(display)");
+            global.compositor.enable_unredirect(global.display);
         }
     }
 }
